1ad693876c0dc8e2b33e5309171b23883b45834e
keenan560 <keenan560@gmail.com>
2021-11-26 16:19:08 -0600

    created react app, installed react router

---
+++ trade-ninja-landing-page/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import "./App.css";
+import Home from "./containers/home/Home";
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
